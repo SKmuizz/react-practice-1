86a484174b2ca5842e1ee1f1dcb15d0c905cde19
Skmuizz <mu4065858@gmail.com>
2024-09-30 19:37:59 +0530

    initial commit

--- practice/src/components/profile.js
+++ practice/src/components/profile.js
@@ -1,11 +1,25 @@
 import React from "react";
-import './App.css';
-import Profile from './components/Profile';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
+const Profile = ({pic,name,us,profileurl}) => {
+  
+    return(
+        
+      // <div className='profile'>
+      //   <p className='name'>my name is {name}</p>
+      //   <img className='profile-image'src={pic} alt={pic}></img>
+      //   <p className='about'>{us}</p>
+      // </div>
 
-const Profile = () => {
-    return (
-        <h1>profile</h1>
+      <Card className="mb-4" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={pic} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text> {us} </Card.Text>
+          <Card.Link href={profileurl} target="_blank" ><Button variant="primary"> {name} visit link</Button></Card.Link>
+        </Card.Body>
+      </Card>
     )
-
 }
+export default Profile;
